75279f4816b91b52e606a4c8e92c544797b3db5d
joshhigg <136753578+joshhigg@users.noreply.github.com>
2024-01-29 15:08:38 -0700

    setup App and fixed server errors

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,10 +5,10 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-              return User.findOne({ _id: context.user._id }).populate('savedBooks');
+                return User.findOne({ _id: context.user._id }).populate('savedBooks');
             }
             throw AuthenticationError;
-          },
+        },
     },
 
     Mutation: {
@@ -16,18 +16,18 @@ const resolvers = {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
-        },       
+        },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
       
             if (!user) {
-              throw AuthenticationError;
+                throw AuthenticationError;
             }
       
             const correctPw = await user.isCorrectPassword(password);
       
             if (!correctPw) {
-              throw AuthenticationError;
+                throw AuthenticationError;
             }
       
             const token = signToken(user);
@@ -47,7 +47,7 @@ const resolvers = {
                 throw new Error('Error saving book')
             }
         },
-        deleteBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             const updatedUser = await User.findOneAndUpdate(
                 { _id: context.user._id },
                 { $pull: { savedBooks: bookId } },
@@ -61,4 +61,6 @@ const resolvers = {
         }
           
     }
-}
+};
+
+module.exports = resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -43,4 +43,4 @@ const typeDefs = `
     }
 `
 
-export default typeDefs;
+module.exports = typeDefs
