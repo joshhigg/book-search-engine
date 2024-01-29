f2c3cfbfc4cf00a6c8d9b4213af7379008efc82a
joshhigg <136753578+joshhigg@users.noreply.github.com>
2024-01-29 13:16:53 -0700

    added resolvers, typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,64 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id }).populate('savedBooks');
+            }
+            throw AuthenticationError;
+          },
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },       
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        saveBook: async (parent, { bookInput }, context) => {
+            try {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookInput } },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            } catch (err) {
+                console.error(err);
+                throw new Error('Error saving book')
+            }
+        },
+        deleteBook: async (parent, { bookId }, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: bookId } },
+                { new: true }
+            );
+            if (!updatedUser) {
+                console.log('No user with this ID')
+                throw new Error('No user with this ID')
+            }
+            return updatedUser;
+        }
+          
+    }
+}
